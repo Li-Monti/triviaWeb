12567767d7b1486fd8fab0820ed6c3d2096d1cd5
Lisandro <lisandrojosemonti@gmail.com>
2025-04-17 11:47:26 -0300

    Correccion de errores ortograficos

--- src/pages/menuPage.jsx
+++ src/pages/menuPage.jsx
@@ -27,29 +27,29 @@ const MenuPage = () => {
             value={value1}
             onChange={(e) => setOption1(e.target.value)}
           >
-            <option value="">Seleccione la categoria</option>
+            <option >Seleccione la categoria</option>
             <option value="9">General Knowledge</option>
             <option value="10">Enterteinment: Books</option>
-            <option value="11">Enterteinment: Music</option>
-            <option value="12">Enterteinment: Musicals & Theatres</option>
-            <option value="13">Enterteinment: Television</option>
-            <option value="14">Enterteinment: Video Games</option>
-            <option value="15">Enterteinment: Board Games</option>
-            <option value="16">Scince & Nature</option>
-            <option value="17">Scince: Computers</option>
-            <option value="18">Scince: Mathematics</option>
-            <option value="19">Mythology</option>
-            <option value="20">Sports</option>
-            <option value="21">Geography</option>
-            <option value="22">History</option>
-            <option value="23">Politics</option>
-            <option value="24">Art</option>
-            <option value="25">Celebrities</option>
-            <option value="26">Animals</option>
-            <option value="27">Vehicles</option>
-            <option value="28">Entreteinment: Comics</option>
-            <option value="29">Animals</option>
-            <option value="30">Scince: Gadgets</option>
+            <option value="11">Enterteinment: Film</option>
+            <option value="12">Enterteinment: Music</option>
+            <option value="13">Enterteinment: Musicals & Theatres</option>
+            <option value="14">Enterteinment: Television</option>
+            <option value="15">Enterteinment: Video Games</option>
+            <option value="16">Enterteinment: Board Games</option>
+            <option value="17">Science & Nature</option>
+            <option value="18">Science: Computers</option>
+            <option value="19">Science: Mathematics</option>
+            <option value="20">Mythology</option>
+            <option value="21">Sports</option>
+            <option value="22">Geography</option>
+            <option value="23">History</option>
+            <option value="24">Politics</option>
+            <option value="25">Art</option>
+            <option value="26">Celebrities</option>
+            <option value="27">Animals</option>
+            <option value="28">Vehicles</option>
+            <option value="29">Entreteinment: Comics</option>
+            <option value="30">Science: Gadgets</option>
             <option value="31">Entreteinment: Japanese Anime & Manga</option>
             <option value="32">Entreteinment: Cartoon & Animations</option>
           </select>
@@ -61,7 +61,7 @@ const MenuPage = () => {
             value={value2}
             onChange={(e) => setOption2(e.target.value)}
           >
-            <option value="">Seleccione la dificultad</option>
+            <option >Seleccione la dificultad</option>
             <option value="easy">Easy</option>
             <option value="medium">Medium</option>
             <option value="hard">Hard</option>
